04d25f73a15e4d36b2014ff393d82a9548be2541
Paddy McGlone <paddymcglone@gmail.com>
2019-02-17 12:46:12 +0000

    Adding dependency injection within our controllers.

--- AspNetCoreTodo/Controllers/TodoController.cs
+++ AspNetCoreTodo/Controllers/TodoController.cs
@@ -5,15 +5,31 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using AspNetCoreTodo.Models;
+using AspNetCoreTodo.Services;
 
 namespace AspNetCoreTodo.Controllers
 {
     public class TodoController : Controller
     {
+        #region Fields
+        ITodoItemService _toDoItemService;
+        #endregion
+
+        #region Constructor
+        public TodoController(ITodoItemService toDoItemService)
+        {
+            _toDoItemService = toDoItemService;   
+        }
+        #endregion
+
+        #region Methods
         public IActionResult Index()
         {
+            var items  = _toDoItemService.GetIncompleteItemsAsync();
+            
             return View();
         }
+        #endregion
 
     }
 }
--- AspNetCoreTodo/Startup.cs
+++ AspNetCoreTodo/Startup.cs
@@ -13,6 +13,7 @@ using Microsoft.EntityFrameworkCore;
 using AspNetCoreTodo.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using AspNetCoreTodo.Services;
 
 namespace AspNetCoreTodo
 {
@@ -43,6 +44,9 @@ namespace AspNetCoreTodo
                 .AddEntityFrameworkStores<ApplicationDbContext>();
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            // Wiring up the new service.
+            services.AddSingleton<ITodoItemService, FakeToDoItemService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
