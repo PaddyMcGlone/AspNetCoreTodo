42a351c87a3e401965351f4c3c03a03bcbb5a487
Paddy McGlone <paddymcglone@gmail.com>
2019-02-19 19:08:00 +0000

    Adding the items migration.

---
+++ AspNetCoreTodo/Data/Migrations/20190219190149_AddingItems.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AspNetCoreTodo.Data.Migrations
+{
+    public partial class AddingItems : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ToDoItems",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(nullable: false),
+                    isDone = table.Column<bool>(nullable: false),
+                    Title = table.Column<string>(nullable: false),
+                    DueAt = table.Column<DateTimeOffset>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ToDoItems", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ToDoItems");
+        }
+    }
+}
