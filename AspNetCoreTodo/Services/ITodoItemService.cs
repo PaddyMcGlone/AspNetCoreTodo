4a3ed085f349a7ed4a800d4e71bb47e848c12953
Paddy McGlone <paddymcglone@gmail.com>
2019-02-27 20:36:29 +0000

    Adding missing files from previous commit.

--- AspNetCoreTodo/Services/ITodoItemService.cs
+++ AspNetCoreTodo/Services/ITodoItemService.cs
@@ -7,7 +7,7 @@ namespace AspNetCoreTodo.Services
 {
     public interface ITodoItemService
     {
-        Task<TodoItem[]> GetIncompleteItemsAsync();
+        Task<TodoItem[]> GetIncompleteItemsAsync(ApplicationUser user);
 
         Task <bool> AddTodoItemsAsync(TodoItem todoItem);       
 
