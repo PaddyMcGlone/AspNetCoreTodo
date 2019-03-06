d80e587cc2104f6997730828a85a695f2a79d828
Paddy McGlone <paddymcglone@gmail.com>
2019-03-06 19:16:02 +0000

    Adding the seed class.

--- AspNetCoreTodo/Seed.cs
+++ AspNetCoreTodo/Seed.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace AspNetCoreTodo
@@ -18,12 +20,32 @@ namespace AspNetCoreTodo
 
         private static async Task EnsureRolesAsync(RoleManager<IdentityRole> RoleManager)
         {
+            var roleExists = await RoleManager.RoleExistsAsync(Constants.AdministratorRole);
+            if(roleExists) return;
 
+            await RoleManager.CreateAsync(new IdentityRole(Constants.AdministratorRole));
         }
 
         private static async Task EnsureTestAdminAsync(UserManager<IdentityUser> UserManager)
         {
+            var testAdmin = await UserManager.Users                
+                .SingleOrDefaultAsync(u => u.Email == "admin@todo.local");
 
+            if(testAdmin == null) return;
+
+            testAdmin = new IdentityUser 
+            {
+                UserName = "admin@todo.local",
+                Email    = "admin@todo.local"
+            };
+
+            await UserManager.CreateAsync(testAdmin, "NotSecure123!!");
+            await UserManager.AddToRoleAsync(testAdmin, Constants.AdministratorRole);
         }
+    }
+
+    public static class Constants 
+    {
+        public const string AdministratorRole = "Administrator";
     }
 }
