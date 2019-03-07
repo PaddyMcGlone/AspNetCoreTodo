4a62b24c498fc904811827302eb4c2b691c9b325
Paddy McGlone <paddymcglone@gmail.com>
2019-03-07 18:12:11 +0000

    Adding the unit tests.

---
+++ AspNetCoreTodo.UnitTests/ToDoItemServiceShould.cs
@@ -0,0 +1,18 @@
+using System.Threading.Tasks;
+using AspNetCoreTodo.Data;
+using AspNetCoreTodo.Services;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+
+namespace AspNetCoreTodo.UnitTests
+{
+    public class ToDoItemServiceShould
+    {
+        [Fact]
+        public async Task AddNewItemAsIncompleteWithDueDate()
+        {
+            //..
+        }
+    }
+
+}
